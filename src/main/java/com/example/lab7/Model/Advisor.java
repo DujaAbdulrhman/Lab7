1b2a2fea41069955b1d60bc68468543320090a0b
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-03-13 19:45:59 +0300

    Update Advisor.java

--- src/main/java/com/example/lab7/Model/Advisor.java
+++ src/main/java/com/example/lab7/Model/Advisor.java
@@ -1,32 +1,3 @@
-/*
-package com.example.lab7.Model;
-
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.ArrayList;
-
-
-@Data
-@AllArgsConstructor
-public class Advaisor {
-    @NotEmpty
-    @Size(min = 2, max = 50)
-    private String AdvaisorName;
-    @NotNull
-    //LET IT ACCEPT ONLY NUMBERS
-    private int numberOfStudents;
-    @Email
-    private String AdvaisorEmail;
-    @NotEmpty
-    private ArrayList<String> Courses;//add get delete update crosses & add a course to student
-}
-*/
 package com.example.lab7.Model;
 
 import jakarta.validation.constraints.*;

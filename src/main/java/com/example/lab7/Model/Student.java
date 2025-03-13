49a9e43acaa1f122ca46411e4c03c39a2e14d20f
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-03-13 19:46:17 +0300

    Update Student.java

--- src/main/java/com/example/lab7/Model/Student.java
+++ src/main/java/com/example/lab7/Model/Student.java
@@ -1,36 +1,3 @@
-/*
-package com.example.lab7.Model;
-
-
-import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.ArrayList;
-
-@Data
-@AllArgsConstructor
-public class Student {
-    @NotEmpty
-    @Size(min = 1, max = 10)
-    private String id;
-    @Email
-    private String email;
-    @NotEmpty
-    @Size(min = 1, max = 20)
-    private String name;
-    @NotNull
-    @Min(5)
-    private double GBA;
-    @NotEmpty @Size(min = 1, max = 200)
-    private String academicPlan;
-    @NotEmpty @Size(min = 1, max = 20)
-    private String advaisorName;
-    @NotEmpty
-    private ArrayList<String> sCourses;
-
-}
-*/
 package com.example.lab7.Model;
 
 import jakarta.validation.constraints.*;
